9e76e6414bace02d8f91dc16e6075d60e7da75cb
Meghan Johns <mjohns1490@gmail.com>
2018-04-04 14:02:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) 
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+function logShout(string)
+{
+  consule.log("hello".toUpperCase)
+   
+}
+
+var uppercase = "HELLO!"
+var lowercase = "hello!"
+var mixedCase = "I love you Grandma."
+
+function sayHiToGrandma(string)
+{
+  if (string === lowercase) {
+  consule.log("I can't hear you!")
+  }
+  else if (string === uppercase) {
+    consule.log("YES INDEED!")
+  }
+else if (string === mixedCase) {
+  consule.log("I love you, too.")
+}
+}
